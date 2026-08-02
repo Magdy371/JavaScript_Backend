3c9f8c1f14a1cb025f854945446fa5b9b7ebe745
Magdy371 <magdyelshrief37@gmail.com>
2026-08-02 19:56:38 +0300

    - Implement checkIfInstanceOf function to verify if a value is an instance of a given class or superclass

--- BackEnd_Express/typeGuard/instance-check.js
+++ BackEnd_Express/typeGuard/instance-check.js
@@ -1,21 +1,27 @@
-function checkIfInstanceOf(obj, classFunction) {
-    if (obj == null || typeof classFunction !== "function") {
+// function checkIfInstanceOf(obj, classFunction) {
+//     if (obj == null || typeof classFunction !== "function") {
+//         return false;
+//     }
+
+//     // Handle primitives that have wrapper classes
+//     if (
+//         (typeof obj === "number" && classFunction === Number) ||
+//         (typeof obj === "string" && classFunction === String) ||
+//         (typeof obj === "boolean" && classFunction === Boolean) ||
+//         (typeof obj === "symbol" && classFunction === Symbol) ||
+//         (typeof obj === "bigint" && classFunction === BigInt)
+//     ) {
+//         return true;
+//     }
+
+//     return obj instanceof classFunction;
+// }
+
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (obj === null || obj === undefined || typeof classFunction !== 'function')
         return false;
-    }
-
-    // Handle primitives that have wrapper classes
-    if (
-        (typeof obj === "number" && classFunction === Number) ||
-        (typeof obj === "string" && classFunction === String) ||
-        (typeof obj === "boolean" && classFunction === Boolean) ||
-        (typeof obj === "symbol" && classFunction === Symbol) ||
-        (typeof obj === "bigint" && classFunction === BigInt)
-    ) {
-        return true;
-    }
-
-    return obj instanceof classFunction;
-}
+    return Object(obj) instanceof classFunction;
+};
 
 // Example runs
 console.log(checkIfInstanceOf(new Date(), Date)); // true
